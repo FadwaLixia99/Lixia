e235076f3182a4283ae3e029735cca42d40e4a4a
FadwaLixia99 <maulyadyfadwa@gmail.com>
2025-01-01 10:44:20 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// Mengarahkan pengguna ke situs web lain
-window.location.href = "https://lixia.vercel.app/";
 
 if (window.location.href !== "https://lixia.vercel.app/") {
     window.location.href = "https://lixia.vercel.app/";
