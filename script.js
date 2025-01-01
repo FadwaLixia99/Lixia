ec2a12a749a589e9dea96406d1b609e3b15638ca
FadwaLixia99 <maulyadyfadwa@gmail.com>
2025-01-01 09:51:37 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-// script.js
 
-// Menampilkan pesan di konsol
-console.log("JavaScript berhasil dihubungkan!");
-
-// Menampilkan pesan alert
-alert("Halo, ini pesan dari file JavaScript eksternal!");
